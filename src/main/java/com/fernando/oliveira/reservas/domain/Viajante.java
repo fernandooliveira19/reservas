1e8cc46058594e9b04ef5b5977b0073483a1f78e
Fernando Oliveira <fer.a.oliveira19@gmail.com>
2022-08-18 10:09:53 -0300

    retirado obrigatoriedade do campo email do viajante

--- src/main/java/com/fernando/oliveira/reservas/domain/Viajante.java
+++ src/main/java/com/fernando/oliveira/reservas/domain/Viajante.java
@@ -35,8 +35,7 @@ public class Viajante implements Serializable {
 	@Size(min=5, max=100, message="O campo nome deve ter entre 5 e 100 caracteres")
 	private String nome;
 	
-	@Column(name="EMAIL", nullable=false)
-	@NotEmpty(message="Preenchimento obrigatório")
+	@Column(name="EMAIL")
 	@Email
 	private String email;
 	
